2ab98b808d4c7772a313858d925e822b4529ea85
alfredo basson <148277029+alfredoBasson1411@users.noreply.github.com>
2024-01-23 22:31:45 +0200

    Challenges
    completed challenges 1,2,3 
    fixed errors
    in js scripts

---
+++ challenge3.js
@@ -0,0 +1,8 @@
+// It is important to show the following message in order to:
+// - assure users
+// - scare hackers
+// - impress investors
+console.warn('Security scan starting');
+
+/* It is important to let users know when they can close the page  */
+console.info('Please wait for the scan to complete before closing the browser.');
